4c7afdbf0c6d051bd4b4a756c37242f06de3d82a
Hakan <35165032+HakanOzr@users.noreply.github.com>
2021-06-14 21:23:01 +0300

    Add files via upload

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/pluralsight/webservice
 
-go 1.16
+go 1.12
--- main.go
+++ main.go
@@ -1,18 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/pluralsight/webservice/controllers"
 )
 
-
-
 func main() {
-
-	fmt.Println("Please navigate to localhost:3000/users to check if the server is up and running")
-
 	controllers.RegisterControllers()
 	http.ListenAndServe(":3000", nil)
 }
